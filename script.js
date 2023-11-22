57a5e1f403494d2aad768628ea97ac0bdbc8d4d1
hapis1703 <hepiss17032@gmail.com>
2023-11-22 20:28:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+AOS.init();
+function changeText() {
+  var replaceText = document.getElementsByClassName("mini-text");
+  replaceText[0].innerHTML = "Scroll untuk melihat";
+  document.getElementById("icon-up").style.display = "none";
+  document.getElementById("icon-down").style.display = "block";
+}
+function dayMode() {
+  document.querySelector("body").style.color = "black";
+  document.querySelector("body").style.backgroundColor = "white";
+  document.querySelectorAll("article").style.backgroundColor = "white";
+}
+function nightMode() {
+  document.querySelector("body").style.color = "white";
+  document.querySelector("body").style.backgroundColor = "black";
+  document.querySelectorAll("article").style.backgroundColor = "black";
+}
+var btnLike = document.querySelector("#green");
+var btnDislike = document.querySelector("#red");
+btnLike.onclick = likeColor;
+btnDislike.onclick = dislikeColor;
+function likeColor() {
+  if (btnDislike.classList.contains("red")) {
+    btnDislike.classList.remove("red");
+  }
+  this.classList.toggle("green");
+}
+function dislikeColor() {
+  if (btnLike.classList.contains("green")) {
+    btnLike.classList.remove("green");
+  }
+  this.classList.toggle("red");
+}
+function changeImage(element) {
+  element.setAttribute("src", "assets/img-header2.png");
+}
+function changeImageBack(element) {
+  element.setAttribute("src", "assets/jumbotron.jpg");
+}
+function changeTitle(element) {
+  element.innerHTML = "Pengembangan Game dan Website";
+}
+function changeTitleBack(element) {
+  element.innerHTML = "Dapatkan Tips Programming Disini!";
+}
+
+var slides = document.querySelectorAll(".slide");
+var buttons = document.querySelectorAll(".slider-btn");
+let currentSlide = 1;
+
+//Manual images slider
+var manualNav = function (manual) {
+  slides.forEach(function (slide) {
+    slide.classList.remove("active");
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
+  slides[manual].classList.add("active");
+  buttons[manual].classList.add("active");
+};
+buttons.forEach(function (btn, i) {
+  btn.addEventListener("click", function () {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
+  let i = 1;
+  var repeater = function () {
+    setTimeout(function () {
+      [...active].forEach(function (activeSlide) {
+        activeSlide.classList.remove("active");
+      });
+      slides[i].classList.add("active");
+      buttons[i].classList.add("active");
+      i++;
+
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 5000);
+  };
+  repeater();
+};
+
+var btnSubmit = document.querySelector(".submit");
+var modal = document.querySelector(".modal-container");
+
+if (btnSubmit) {
+  btnSubmit.addEventListener("click", function () {
+    modal.classList.add("show");
+    const next = document.querySelector(".next");
+    next.setAttribute("value", "http://localhost:3000/contact.html");
+  });
+}
